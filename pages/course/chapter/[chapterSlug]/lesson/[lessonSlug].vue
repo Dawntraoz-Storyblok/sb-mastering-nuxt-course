e54c1d6e18937feee9baa64eaaece819d5e431d2
Dawntraoz <albasilventefu@gmail.com>
2023-01-13 11:54:43 +0100

    session-1: First learning session from 1.0 to 1.5

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Lesson</h2>
+  <p>This is a lesson</p>
+  <p>{{ chapter.title }}</p>
+  <p>{{ lesson.title }}</p>
+</template>
+
+<script setup>
+const course = useCourse();
+const route = useRoute();
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+</script>
