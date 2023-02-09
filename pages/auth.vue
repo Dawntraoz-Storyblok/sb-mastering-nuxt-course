8851b1b6c0857ed856f77c539d05da96dfe21a6c
ravitemer <ravitemer@gmail.com>
2023-02-09 02:36:43 +0000

    fixed input fields

--- pages/auth.vue
+++ pages/auth.vue
@@ -11,13 +11,13 @@
           <label class="label">
             <span class="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" class="input input-bordered" />
+          <input type="email" placeholder="email" class="input input-bordered" />
         </div>
         <div class="form-control">
           <label class="label">
             <span class="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" class="input input-bordered" />
+          <input type="password" placeholder="password" class="input input-bordered" />
           <label class="label">
             <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
           </label>
